54d52b0a0869566343a356817386340da07fe3b0
nike-17 <nikita@groshin.com>
2025-11-01 10:55:18 -0700

    Remove node_modules, .idea, and dist from version control

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare class RussianToJSTranspiler {
-    transpile(source: string): string;
-    transpileFile(inputFile: string, outputFile?: string): void;
-}
-export declare const transpiler: RussianToJSTranspiler;
-export declare function transpile(source: string): string;
-export {};
